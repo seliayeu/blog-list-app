ce11eea682005c8c2856a6a008b370e6d87db13e
zloopo <seliayeu@ualberta.ca>
2021-02-13 20:04:02 -0700

    test mongoDB connection bug

--- server/app.js
+++ server/app.js
@@ -28,9 +28,8 @@ mongoose.connect(config.MONGODB_URI,
   .catch((error) => {
     logger.error('error connecting to MongoDB:', error.message);
   });
-app.use(express.static('dist'))
 app.use(cors());
-app.use(express.static('build'));
+app.use(express.static('dist'));
 app.use(express.json());
 app.use(middleware.requestLogger);
 app.use(middleware.getTokenFrom);
